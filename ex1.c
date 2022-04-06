d9052c66ed1b808e9c3fe024402c09ae2f9c1958
jirvuuu <103093155+jirvuuu@users.noreply.github.com>
2022-04-06 12:56:11 +0800

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int numbers;
+
+    printf("Enter number: ");
+    scanf("%d",&numbers);
+
+    printf("Hello World %d!\n",numbers);
+
+    return 0;
+}
